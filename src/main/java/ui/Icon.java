92a44a0b73cc8017f15d8ce07ce8b37f5090bd7b
Humberto Pinheiro <humbhenri@gmail.com>
2015-04-17 20:51:09 -0300

    icon selectable behaviour

--- src/main/java/ui/Icon.java
+++ src/main/java/ui/Icon.java
@@ -4,6 +4,9 @@ import model.Directory;
 import model.File;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 /**
  * Created by humberto on 16/04/2015.
@@ -17,17 +20,36 @@ public class Icon extends JLabel {
         super(directory.toString());
         setIcon(folderIcon);
         putTextBelowIcon();
+        addSelectBehaviour();
     }
 
     public Icon(File file) {
         super(file.getFilename(), JLabel.CENTER);
         setIcon(fileIcon);
         putTextBelowIcon();
+        addSelectBehaviour();
     }
 
     private void putTextBelowIcon() {
         setHorizontalTextPosition(JLabel.CENTER);
         setVerticalTextPosition(JLabel.BOTTOM);
+    }
+
+    private void addSelectBehaviour() {
+        setOpaque(true);
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                SwingUtilities.invokeLater(() -> {
+                    for (Component component : getParent().getComponents()) {
+                        if (component instanceof Icon) {
+                            component.setBackground(UIManager.getColor("InternalFrame.background"));
+                        }
+                    }
+                    setBackground(UIManager.getColor("TextField.selectionBackground"));
+                });
+            }
+        });
     }
 
 }

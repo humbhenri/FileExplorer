e6e6abafc81b110356ac1cafe669268fbdda8fbc
Humberto Pinheiro <humbhenri@gmail.com>
2015-04-17 20:25:34 -0300

    sort directories first

--- src/main/java/model/Directory.java
+++ src/main/java/model/Directory.java
@@ -1,11 +1,9 @@
 package model;
 
 import java.io.IOException;
-import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
+import java.util.Comparator;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
@@ -23,10 +21,23 @@ public class Directory implements FileSystemEntity {
     public Stream<FileSystemEntity> list() {
         try {
             return StreamSupport.stream(Files.newDirectoryStream(pwd).spliterator(), false)
+                    .sorted(sortByDirectoriesFirst())
                     .map(FileFactory::create);
         } catch (IOException e) {
             return Stream.empty();
         }
+    }
+
+    private Comparator<Path> sortByDirectoriesFirst() {
+        return (path1, path2) -> {
+            java.io.File file1 = path1.toFile();
+            java.io.File file2 = path2.toFile();
+            if (file1.isDirectory() && !file2.isDirectory())
+                return -1;
+            if (!file1.isDirectory() && file2.isDirectory())
+                return 1;
+            return path1.compareTo(path2);
+        };
     }
 
     @Override
